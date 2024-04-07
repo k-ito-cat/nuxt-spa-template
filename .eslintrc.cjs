317d0ece41a324f275129153e420372db905f158
kyo212 <kyohy163@gmail.com>
2024-04-07 13:58:43 +0900

    chore: eslint, prettierの設定ファイル記述

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true },
+  extends: [
+    '@nuxt/eslint-config',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+  ],
+  plugins: ['vue', 'unused-imports'],
+  rules: {
+    'no-unused-vars': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+      },
+    ],
+    'vue/max-attributes-per-line': 0,
+    'vue/valid-template-root': 'off',
+  },
+};
