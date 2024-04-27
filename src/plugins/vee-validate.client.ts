e99f6d0c838c28b652b8012725162c8527f837a9
k-ito <kyohy163@gmail.com>
2024-04-27 14:58:52 +0900

    refactor: フォーマット差分

--- src/plugins/vee-validate.client.ts
+++ src/plugins/vee-validate.client.ts
@@ -1,11 +1,11 @@
-import _ from "lodash";
-import { setLocale, type LocaleObject } from "yup";
-import { suggestive } from "yup-locale-ja";
+import _ from 'lodash';
+import { setLocale, type LocaleObject } from 'yup';
+import { suggestive } from 'yup-locale-ja';
 
 export default defineNuxtPlugin(() => {
   const customLocaleObject: LocaleObject = {
     mixed: {
-      required: ({ label }) => (label ? label + "を" : "") + "入力してください",
+      required: ({ label }) => (label ? label + 'を' : '') + '入力してください',
     },
   };
 
